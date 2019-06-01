5d79f02deb137789e372078cff4c3746e67d0e03
tefaafigueroa <tefaafigueroa@gmail.com>
2019-06-01 17:02:20 -0600

    cambios2

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
-var pokemon = angular.module("pokemon",[])
-
-pokemon.controller("Pokedex",function($scope,$rootScope,$http){
-
-	$scope.pokedexB = [];
-	for (var i = 1; i < 100; i++) {
-
-			$http({
-			method:"GET",
-			url:"https://pokeapi.co/api/v2/pokemon/1"+i
-		}).then(function callbacksucess(y){
-			$scope.pokedexB.push(y)
-		})
+var Pokemon = angular.module("Pokemon",[])
+Pokemon.controller("ListadoPokemon",function($scope,$rootScope,$http){
+	$scope.Lista = [];
+	for(var y=1; y<=100;y++){
+		$http({
+		metho:"GET",
+		url:"https://pokeapi.co/api/v2/pokemon/"+y
+	}).then(function data(x){
+		$scope.Lista.push(x);
+	})
 	}
-})
+	
+});
