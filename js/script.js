22057b638c140326f5e03450fc5aff9e072602e1
tefaafigueroa <tefaafigueroa@gmail.com>
2019-06-06 10:17:46 -0600

    cambios4

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var Pokemon = angular.module("Pokemon",[])
 Pokemon.controller("ListadoPokemon",function($scope,$rootScope,$http){
 	$scope.Lista = [];
-	for(var y=1; y<=100;y++){
+	for(var y=1; y<=900;y++){
 		$http({
 		metho:"GET",
 		url:"https://pokeapi.co/api/v2/pokemon/"+y
